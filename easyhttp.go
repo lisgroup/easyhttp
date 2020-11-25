b63bde283bdd482da0de78ba6204a787c046c3bd
lisgroup <lisgroup@qq.com>
2020-11-25 13:54:04 +0800

    chore: 增加构造方法和 Options 选项入参

--- easyhttp.go
+++ easyhttp.go
@@ -13,44 +13,56 @@ import (
 
 // Client 结构体
 type Client struct {
+	options Options
+}
+
+// Options 结构体
+type Options struct {
 	BaseURI  string
 	Timeout  float32
 	Headers  map[string]interface{}
 	BodyMaps map[string]interface{}
 }
 
+// Client 构造函数
+func NewClient(opts ...Options) *Client {
+	var client = Client{}
+	if len(opts) > 0 {
+		client.options = opts[0]
+	}
+	return &client
+}
+
 // Request 简单封装一个Http请求方法
 // rawUrl 请求的接口地址
 // method 请求的方法，GET/POST
-// opt 可选参数
-func (c *Client) Request(rawUrl, method string, cli ...Client) (result string, err error) {
-	if len(cli) > 0 {
-		c.BaseURI = cli[0].BaseURI
-		c.Timeout = cli[0].Timeout
-		c.Headers = cli[0].Headers
-		c.BodyMaps = cli[0].BodyMaps
+// options 可选参数
+func (c *Client) Request(rawUrl, method string, options ...Options) (result string, err error) {
+	if len(options) > 0 {
+		c.options = options[0]
 	}
 	// 默认超时 20 秒
-	if c.Timeout <= 0 {
-		c.Timeout = 20
+	if c.options.Timeout <= 0 {
+		c.options.Timeout = 20
 	}
 
 	client := &http.Client{
-		Timeout: time.Duration(c.Timeout*1000) * time.Millisecond,
+		Timeout: time.Duration(c.options.Timeout*1000) * time.Millisecond,
 	}
 	// 请求的 body 内容
 	// 判断headers头信息是否存在 content-type:application/json
 	var isJson bool
-	for key, value := range c.Headers {
+	for key, value := range c.options.Headers {
 		val := fmt.Sprintf("%v", value)
 		if strings.ToLower(key) == "content-type" && strings.ToLower(val) == "application/json" {
 			isJson = true
+			break
 		}
 	}
 
 	var sendBody io.Reader
 	if isJson {
-		res, errJson := json.Marshal(c.BodyMaps)
+		res, errJson := json.Marshal(c.options.BodyMaps)
 		if errJson != nil {
 			err = errJson
 			return
@@ -58,7 +70,7 @@ func (c *Client) Request(rawUrl, method string, cli ...Client) (result string, e
 		sendBody = bytes.NewBuffer(res)
 	} else {
 		data := url.Values{}
-		for key, value := range c.BodyMaps {
+		for key, value := range c.options.BodyMaps {
 			// interface 转 string
 			data.Set(key, fmt.Sprintf("%v", value))
 		}
@@ -69,17 +81,16 @@ func (c *Client) Request(rawUrl, method string, cli ...Client) (result string, e
 	if method != "GET" && method != "POST" {
 		method = "GET"
 	}
-	if c.BaseURI != "" {
-		rawUrl = c.BaseURI + rawUrl
+	if c.options.BaseURI != "" {
+		rawUrl = c.options.BaseURI + rawUrl
 	}
 	// 提交请求
-	request, err1 := http.NewRequest(method, rawUrl, sendBody) // URL-encoded|JSON payload
-	if err1 != nil {
-		err = err1
+	request, err := http.NewRequest(method, rawUrl, sendBody) // URL-encoded|JSON payload
+	if err != nil {
 		return
 	}
 	// 增加header头信息
-	for key, val := range c.Headers {
+	for key, val := range c.options.Headers {
 		request.Header.Set(key, fmt.Sprintf("%v", val))
 	}
 	// 处理返回结果
@@ -109,11 +120,11 @@ func (c *Client) Request(rawUrl, method string, cli ...Client) (result string, e
 }
 
 // Get 请求方式
-func (c *Client) Get(rawUrl string, cli ...Client) (result string, err error) {
-	return c.Request(rawUrl, "GET", cli...)
+func (c *Client) Get(rawUrl string, options ...Options) (result string, err error) {
+	return c.Request(rawUrl, "GET", options...)
 }
 
 // Post 请求方式
-func (c *Client) Post(rawUrl string, cli ...Client) (result string, err error) {
-	return c.Request(rawUrl, "POST", cli...)
+func (c *Client) Post(rawUrl string, options ...Options) (result string, err error) {
+	return c.Request(rawUrl, "POST", options...)
 }
